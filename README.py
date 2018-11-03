845c8f7d5b176ae9e16edbbd82a71ef2a95b4fe8
snehitvaddi <35267863+snehitvaddi@users.noreply.github.com>
2018-11-03 13:23:22 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,12 +1,18 @@
 # simple-python-email-sending-code
-A simple code to send automatic emails direcly from python
+# A simple code to send automatic emails direcly from python
+ #In order to Send emails through python3 , we use SMTP Serever libraries.
+
+ #I want to see an acknowledgement if eamil has sent else notify me !!.. 
 
 import smtplib
 sender = 'v.snehith999@gmailcom'
 receiver = 'v.pujithj@gmailcom'
 message=""" From <email of sender> 
   To: To person <email of receiver>
-  subject: SMTP Automatic mail"""
+  subject: SMTP Automatic mail
+  <h>THIS IS HEAD</h>
+  <b>THSI IS BODY</b>
+  """
   
 #Note: (""")indicate multiline string.
 
