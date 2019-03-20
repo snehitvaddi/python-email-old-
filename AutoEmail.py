962146b9c2bf51e8d4283240dea398b04101b1c6
Snehit Vaddi <35267863+snehitvaddi@users.noreply.github.com>
2019-03-20 23:02:22 +0530

    Create AutoEmail.py

---
+++ AutoEmail.py
@@ -0,0 +1,19 @@
+import smtplib
+sender = 'v.snehith999@gmailcom'
+receiver = 'v.pujithj@gmailcom'
+message=""" From <email of sender> 
+  To: To person <email of receiver>
+  subject: SMTP Automatic mail
+  <h>THIS IS HEAD</h>
+  <b>THSI IS BODY</b>
+  """
+  
+#Note: (""")indicate multiline string.
+
+try:
+ server=smtplib.SMTP('smtp.gmail.com',587)
+ server.sendmail(sender,receiver,message)
+ print('Main has been sent successfully')
+except SMTPException:
+ print('Unable to send mail')
+ 
